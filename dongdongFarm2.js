be53c34302f8f33a8d591c21d86b82801a8962b5
Tsu1m <128674303+Tsu1m@users.noreply.github.com>
2024-10-22 11:29:02 +0800

    Update dongdongFarm2.js

--- dongdongFarm2.js
+++ dongdongFarm2.js
@@ -6,7 +6,7 @@ console.log('"asdfasdfasdfasdf')
 
 console.log(process.env.JD_KEY_CK)
 
-console.log(process.env.APP_COOKIE[0])
+console.log(process.env.APP_COOKIE)
 
 console.log(process.env.JD_KEY_CK2)
 
